ee4556389e14e1d3924a2fe4d72623ee161eb025
Trayson Kala Keli'i <traysonkelii@gmail.com>
2019-01-02 20:38:25 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0px !important;
+  background-color: black;
+}
+</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>Contact page</h1>
+    <h3>Coming soon...</h3>
+    <p>email: traysonkelii@gmail.com</p>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      msg: 'This is the contact page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2, h3 {
+  font-weight: normal;
+  color:rgb(89,144,212);
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+  color:rgb(89,144,212);
+}
+a {
+  color: #42b983;
+}
+p{
+  color:rgb(89,144,212);
+}
+</style>
